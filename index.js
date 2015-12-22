8cb5b40195c84b536a9934f6a438ed4595afc927
Matt Styles <matt@veryfizzyjelly.com>
2015-12-22 16:04:25 +0000

    #add get rendering

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
 var gl = require( 'webgl-context' )()
+
 var Loop = require( 'canvas-loop' )
 var Shader = require( 'gl-basic-shader' )
 var SpriteBatch = require( 'gl-sprite-batch' )
+var Texture = require( 'gl-texture2d' )
+
+var baboon = require( 'baboon-image' )
+var mat4 = require( 'gl-mat4')
+
 
 var canvas = gl.canvas
 document.body.appendChild( canvas )
@@ -16,7 +22,7 @@ app.start()
 app.on( 'tick', render )
 
 var shader = Shader( gl, {
-  texCoord: true,
+  texcoord: true,
   color: true,
   normal: false
 })
@@ -26,6 +32,39 @@ var batch = SpriteBatch( gl, {
   capacity: 100   // default
 })
 
+var texture = Texture( gl, baboon )
+
+var ortho = mat4.create()
+var width = app.shape[ 0 ]
+var height = app.shape[ 1 ]
+
 function render( dt ) {
-  
+
+  gl.enable( gl.BLEND )
+  gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA )
+
+  shader.bind()
+  shader.uniforms.texture0 = 0
+
+  mat4.ortho( ortho, 0, width, height, 0, 0, 1 )
+  shader.uniforms.projection = ortho
+
+  batch.clear()
+  batch.bind( shader )
+
+  batch.push({
+    position: [ 0, 0 ],
+    shape: [ 128, 128 ],
+    color: [ 1, 1, 1, 1 ],
+    texture: texture
+  })
+
+  batch.draw()
+
+  batch.unbind()
 }
+
+
+//debug
+window.app = app
+window.gl = gl
